0173888a7abb4b48b3029a4afdedb09322332c8e
araujooj <gabrielrudy575@gmail.com>
2020-05-17 13:44:02 -0300

    chore: added cors, and better relation on Models

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import 'dotenv/config';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
@@ -10,6 +11,7 @@ createConnection();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
